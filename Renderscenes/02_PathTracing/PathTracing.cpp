2f43551a979dbedc5cc705adf7c688e7c1058e45
MasterMoritz <andreas.moritz@student.uibk.ac.at>
2015-12-21 17:00:19 +0100

    add transluscent sphere for testing

--- Renderscenes/02_PathTracing/PathTracing.cpp
+++ Renderscenes/02_PathTracing/PathTracing.cpp
@@ -318,6 +318,7 @@ Sphere spheres[] =
 
     Sphere(16.5, Vector(27, 16.5, 47), Vector(), Vector(1,1,1)*.999,  SPEC), /* Mirror sphere */
     Sphere(10.5, Vector(50, 16.5, 105), Vector(), Vector(1,1,1)*.999,  GLOSSY), /* Glossy sphere */
+	Sphere(10.5, Vector(10.5, 50.0, 105), Vector(), Vector(1,1,1)*.999,  TRANS), /* Transluscent sphere */
     Sphere(16.5, Vector(73, 16.5, 78), Vector(), Vector(1,1,1)*.999,  REFR), /* Glas sphere */
 
     Sphere( 1.5, Vector(50, 81.6-16.5, 81.6), Vector(4,4,4)*100, Vector(), DIFF), /* Light */
